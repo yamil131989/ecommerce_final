7bb253cebbea3a6c741b4043ac3fb08d25141ada
yamil131989 <152355713+yamil131989@users.noreply.github.com>
2024-01-28 20:53:40 -0300

    Update productos.js

--- JS/productos.js
+++ JS/productos.js
@@ -1,1 +1,73 @@
+const productos=[
+    {id:1,
+    nombre: "Samsung Galaxy S22 8gb + 128gb  color verde",
+    precio: 870999,    
+    imagen:"https://http2.mlstatic.com/D_NQ_NP_2X_895723-MLU72854906612_112023-F.webp",
+    },
+    {id:2,
+    nombre: "Samsung Galaxy S23 128gb Color Phantom black",
+    precio: 999999,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_2X_654151-MLA53448775145_012023-F.webp",
+    },
 
+    {id:3,
+    nombre: "Motorola Razr 40 256gb Gris Mate + vidrio templado",
+    precio: 1898550,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_2X_717512-MLA73805462339_012024-F.webp",
+    },
+    
+    {id:4,
+    nombre:"Motorola Edge 30 Fusion Ram 12gb 256gb 50mp",
+    precio: 2095123,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_2X_694122-MLA70783036076_082023-F.webp",
+    },
+    
+    {id:5,
+    nombre:"Xiaomi 13T Pro Dual SIM 512 GB negro 12 GB RAM ",   
+    precio: 1499999,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_2X_960171-MLA72507542179_102023-F.webp",
+    },
+
+    {id:6,
+    nombre:"Apple iPhone 15 Pro (1 Tb) Negro - 8 E Sim   ",
+    precio: 40000000,
+    imagen:"https://http2.mlstatic.com/D_NQ_NP_2X_719768-MLA71571849121_092023-F.webp",
+    },
+
+    {id:7,
+    nombre:"Apple iPhone 14 Pro Max (1 Tb) - Negro - Dual Sim",
+    precio:5999999,
+    imagen:" https://http2.mlstatic.com/D_NQ_NP_2X_856715-MLA71381558194_082023-F.webp",
+    },
+    
+    {id:8,
+    nombre:" Sony Xperia 1 V Celular 12gb Ram 256 Gb 2 Chips",
+    precio: 3523405,
+    imagen:"https://http2.mlstatic.com/D_NQ_NP_2X_947145-MLA71903201667_092023-F.webp", 
+    }, 
+
+    {id:9,
+    nombre:"Samsung Galaxy Z Fold5 5g Dual-sim 256 Gb / 12 Gb Ram Blue",
+    precio:3459999,
+    imagen:" https://http2.mlstatic.com/D_NQ_NP_2X_749682-MLA71025450539_082023-F.webp",
+    },
+
+    {id:10,
+    nombre:" Huawei Mate 30 Pro Dual SIM 256 GB púrpura cósmico 8 GB RAM",
+    precio: 2499999,
+    imagen:" https://http2.mlstatic.com/D_NQ_NP_2X_763167-MLA41813090445_052020-F.webp",
+    },
+
+    {id:11,
+    nombre:" Xiaomi Redmi Note 11 4gb Ram 128gb Rom Color Azul",
+    precio: 250000,
+    imagen:" https://http2.mlstatic.com/D_NQ_NP_2X_743030-MLA73861678751_012024-F.webp",
+    },
+    
+    {id:12,
+    nombre:"CAT S62 Pro Dual SIM 128 GB negro 6 GB RAM",
+    precio:1149990,
+    imagen:"https://http2.mlstatic.com/D_NQ_NP_2X_794588-MLA73064621289_112023-F.webp",
+    }
+    
+]
